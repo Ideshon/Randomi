e2cdecc04878dea5d67f8a6d149634f8b7742b3b
Nadagh <unsolved.magnet@mail.ru>
2024-03-14 16:09:28 +0500

    add comment

--- Randomi_1.1.1.py
+++ Randomi_1.1.1.py
@@ -1,13 +1,15 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel, QLineEdit, QHBoxLayout, QSplitter
-from text_randomizer import TextRandomizer
 from re import sub  # стандартная библиотека для работы с текстом
-from PyQt5.QtCore import QSettings # для сохранения и загрузки
+
+from PyQt5.QtCore import QSettings  # для сохранения и загрузки
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel, QLineEdit, QHBoxLayout
+from text_randomizer import TextRandomizer
+
 
 class TextRandomizerGUI(QWidget):
     def __init__(self):
-        super().__init__()
-        self.settings = QSettings('Randomi', 'TextRandomizerGUI') # Инициализация QSettings
-        self.initUI() # интерфейс
+        super().__init__()  # запускаем инициализатор родительского класса
+        self.settings = QSettings('Randomi', 'TextRandomizerGUI')  # Инициализация QSettings
+        self.initUI()  # интерфейс
         self.loadSettings()  # Загрузка сохраненных настроек при инициализации
 
         self.delimeter.setMinimumSize(0, 20)  # Задает минимальный размер 0x20 пикселей
@@ -17,36 +19,38 @@ class TextRandomizerGUI(QWidget):
         self.result_output.setMinimumSize(200, 20)
         self.randomize_button.setMaximumSize(10000, 80)
 
+
     def initUI(self):
         self.layout = QVBoxLayout()
-        self.delimeterL = QLabel('Delimeter',self)
+        self.delimeterL = QLabel('Delimeter', self)
         self.entry = QTextEdit(self)  # Используется для многострочного ввода
-        self.delimeter = QLineEdit(';', self) # Используется для однострочного ввода
+        self.delimeter = QLineEdit(';', self)  # Используется для однострочного ввода
         self.randomize_button = QPushButton('Randomize', self)
         self.result_output = QTextEdit(self)
         self.result_output.setReadOnly(False)  # Сделать поле только для чтения
 
         # подсказка
         self.template_label = QTextEdit(
-            '\n\nКоманда "Синонимы" - {variant1 | variant2 | variant3} - вставляет один из вариантов в результирующую строку'
-            '\nЕсли вы хотите пропустить текст - используйте вариант "пусто" - {|variant}'
-            '\nКоманда Mixin = [ текст 1 | текст 2 | текст 3] - будет случайным образом миксовать эти варианты'
-            '\nВы можете использовать разделитель в mixin - [+,+text 1|text2 ] - вы получите text2,text1. Разделитель может быть любым символом или набором символов: [+==+ a|b] - a==b или b==a'
-            '\nЕсли вы хотите получить специальный символ в вашем результате (\{\, \}\, \[\, \]\, \|\, \+\) - используйте обратный слеш для него - {, }, [, ], |, \+'
-            '\nВсе эти команды могут быть смешаны и вложены во все комбинации: \'начало {aa|bb|{cc1|cc2}} или [a1|{word1|word2}|a3| [aa1|aa2|aa3]]\''
-            '\nВы можете использовать специальные предопределенные функции рандомизации в шаблонах - {случайное целое = $RANDINT(1,10), uuid = $UUID, сейчас = $NOW(%Y-%M-%d)}.'
-            '\nРезультат будет = \'случайное целое = 4, uuid = 8ae6bdf4-d321-40f6-8c3c-81d20b158acb, сейчас = 2017-08-01\''
-            '\nВы можете определить свои собственные функции рандомизации и использовать их в шаблонах. Читайте о функциях дальше.'
-            , self)
+                '\n\nКоманда "Синонимы" - {variant1 | variant2 | variant3} - вставляет один из вариантов в результирующую строку'
+                '\nЕсли вы хотите пропустить текст - используйте вариант "пусто" - {|variant}'
+                '\nКоманда Mixin = [ текст 1 | текст 2 | текст 3] - будет случайным образом миксовать эти варианты'
+                '\nВы можете использовать разделитель в mixin - [+,+text 1|text2 ] - вы получите text2,text1. Разделитель может быть любым символом или набором символов: [+==+ a|b] - a==b или b==a'
+                '\nЕсли вы хотите получить специальный символ в вашем результате (\{\, \}\, \[\, \]\, \|\, \+\) - используйте обратный слеш для него - {, }, [, ], |, \+'
+                '\nВсе эти команды могут быть смешаны и вложены во все комбинации: \'начало {aa|bb|{cc1|cc2}} или [a1|{word1|word2}|a3| [aa1|aa2|aa3]]\''
+                '\nВы можете использовать специальные предопределенные функции рандомизации в шаблонах - {случайное целое = $RANDINT(1,10), uuid = $UUID, сейчас = $NOW(%Y-%M-%d)}.'
+                '\nРезультат будет = \'случайное целое = 4, uuid = 8ae6bdf4-d321-40f6-8c3c-81d20b158acb, сейчас = 2017-08-01\''
+                '\nВы можете определить свои собственные функции рандомизации и использовать их в шаблонах. Читайте о функциях дальше.'
+                , self
+                )
         self.template_label.setReadOnly(False)
 
-        self.randomize_button.clicked.connect(self.randomize_text) # Кнопка
+        self.randomize_button.clicked.connect(self.randomize_text)  # Кнопка
 
         # горизонтальный компоновщик для размещения метки и поля ввода разделителя рядом
         self.hbox = QHBoxLayout()
         self.hbox.addWidget(self.delimeterL)
         self.hbox.addWidget(self.delimeter)
-        self.hbox.addStretch(1) # Выравнивание по левому
+        self.hbox.addStretch(1)  # Выравнивание по левому
 
         # перемещены в горизонт
         # self.layout.addWidget(self.delimeterL)
@@ -60,33 +64,38 @@ class TextRandomizerGUI(QWidget):
         self.layout.addWidget(self.template_label)
 
         self.setLayout(self.layout)
-        self.setWindowTitle('Randomi') # название окна
+        self.setWindowTitle('Randomi')  # название окна
+
 
     # функция нормализации примера (для , ->  1,2,3 в {1|2|3})
-    def normalize_template(self, template,delim):
+    def normalize_template(self, template, delim):
         norm_template = sub(f"{delim}", "|", template)
         return norm_template
+
+
     def randomize_text(self):
         try:
             template = self.entry.toPlainText()  # Получение текста из QTextEdit
-            norm_template=self.normalize_template(template,self.delimeter.text())
+            norm_template = self.normalize_template(template, self.delimeter.text())
             text_rnd = TextRandomizer(norm_template)
             self.result_output.setText(text_rnd.get_text())  # Вывод результата в QTextEdit
         except Exception as e:
             self.result_output.setText(f"Error: {str(e)}")  # Вывод ошибки в QTextEdit
 
-    def loadSettings(self): # загрузка
+
+    def loadSettings(self):  # загрузка
         self.template_label.setText(self.settings.value('template_label', ''))
         self.entry.setPlainText(self.settings.value('entry', ''))
         self.result_output.setText(self.settings.value('result_output', ''))
 
-    def saveSettings(self): # сохранение
+
+    def saveSettings(self):  # сохранение
         self.settings.setValue('template_label', self.template_label.toPlainText())
         self.settings.setValue('entry', self.entry.toPlainText())
         self.settings.setValue('result_output', self.result_output.toPlainText())
 
 
-    def closeEvent(self, event): # действие при закрытии
+    def closeEvent(self, event):  # действие при закрытии
         self.saveSettings()
         super().closeEvent(event)
 
