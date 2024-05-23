2bea0fbd4f21a2f39c80085dffcfe1af91ee3f18
Ideshon <54465640+Ideshon@users.noreply.github.com>
2024-05-23 10:20:54 +0500

    Delete main.py
    Signed-off-by: Ideshon <54465640+Ideshon@users.noreply.github.com>

--- main.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from gui import TextRandomizerGUI
-
-if __name__ == '__main__':
-    app = QApplication([])
-    window = TextRandomizerGUI()
-    window.show()
-    app.exec_()
